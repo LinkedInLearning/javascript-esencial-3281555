e3a5f5dd0c34ba4fa0d3127e3edd2a023ab77ee3
Natalia C <46870371+nlunaty@users.noreply.github.com>
2023-09-20 12:57:10 +0100

    Update leccion_01_primitivos_no_primitivos.js

--- Unidad02/leccion_01_primitivos_no_primitivos.js
+++ Unidad02/leccion_01_primitivos_no_primitivos.js
@@ -1,1 +1,27 @@
-console.log('hola')
+// Tipos primitivos:
+
+// números:
+const valorNumeral = 1000;
+
+//strings:
+const valorString = 'Hola JavaScript';
+
+//booleanos:
+const valorBooleano = true;
+
+//Symbol:
+const valorSymbol = Symbol();
+
+//Indefinidos:
+const valorIndefinido = undefined;
+
+//Nulos:
+const valorNulo = null;
+
+// Tipos no primitivos:
+
+// Objetos:
+const objeto = { nombre: 'Natalia' };
+
+//Arreglos:
+const arr = [1, 2, 3, 4, 5];
