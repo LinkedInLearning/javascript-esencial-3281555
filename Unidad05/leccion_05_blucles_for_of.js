c2d21c4529ea0c60016615925c5cbbcb04fe690c
Natalia C <46870371+nlunaty@users.noreply.github.com>
2023-10-04 18:23:26 +0100

    update

---
+++ Unidad05/leccion_05_blucles_for_of.js
@@ -0,0 +1,13 @@
+// Iteración sobre arreglo:
+let colores = ["rojo", "verde", "azul"];
+
+for (let color of colores) {
+  console.log(color);
+}
+
+// Iteración sobre los caracteres de un string:
+let saludo = "Buenos días";
+
+for (let caracter of saludo) {
+  console.log(`El carácter actual es '${caracter}'`);
+}
