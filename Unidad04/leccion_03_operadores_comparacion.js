9e7b8b0219e27c250c80afb4274a3bd2e11eb4d5
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2023-10-04 23:21:23 +0100

    fixes

--- Unidad04/leccion_03_operadores_comparacion.js
+++ Unidad04/leccion_03_operadores_comparacion.js
@@ -1,16 +1,16 @@
-// Operadores de compraración:
+// Operadores de comparación:
 
 let x = 5;
 let y = '5';
 
 // Operador de igualdad:
-console.log(x == y); // Salida: true (after type coercion)
+console.log(x == y); // Salida: true (después de la coerción de tipos)
 
 // Operador de igualdad estricta:
 console.log(x === y); // Salida: false
 
 // Operador de no igualdad:
-console.log(x != y); // Salida: false (after type coercion)
+console.log(x != y); // Salida: false (después de la coerción de tipos)
 
 // Operador de no igualdad estricta:
 console.log(x !== y); // Salida: true
