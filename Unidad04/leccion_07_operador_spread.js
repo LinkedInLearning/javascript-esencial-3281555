dadbaf728f02ed9db2a86f60bc6d3f1136fc023b
Natalia C <46870371+nlunaty@users.noreply.github.com>
2023-10-06 01:00:46 +0100

    extra lesson

---
+++ Unidad04/leccion_07_operador_spread.js
@@ -0,0 +1,34 @@
+
+// Copiar un arreglo o un objeto:
+
+let arregloOriginal = [1, 2, 3];
+let copiaArreglo = [...arregloOriginal];
+console.log(copiaArreglo);
+
+let objetoOriginal = { a: 1, b: 2 };
+let copiaObjeto = { ...objetoOriginal };
+console.log(copiaObjeto);
+
+
+// Unir arreglos
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let unidos = [...array1, ...array2];
+console.log(unidos);
+
+// Pasar argumentos a una función:
+function suma(a, b, c) {
+  return a + b + c;
+}
+
+let numeros = [1, 2, 3];
+let resultado = suma(...numeros);
+console.log(resultado);
+
+// Convirtiendo un string en un arreglo de caracteres:
+let str = 'Hola!, ';
+let caracteres = [...str];
+console.log(caracteres);
+
+
+
--- Unidad08/leccion_01_generando_arreglos.js
+++ Unidad08/leccion_01_generando_arreglos.js
@@ -1,1 +1,4 @@
-console.log('hola')
+let fruits = ['apple', 'banana', 'cherry'];
+let numbers = new Array(1, 2, 3, 4, 5);
+let range = Array.from({ length: 5 }, (_, index) => index + 1);
+let colors = ['red', 'green', ...['blue', 'yellow']];
