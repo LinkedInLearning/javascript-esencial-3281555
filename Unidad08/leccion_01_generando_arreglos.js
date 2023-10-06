fbfe543753d7f902e06628c7299abd1e8465139f
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2023-10-06 15:48:36 +0100

    fix

--- Unidad08/leccion_01_generando_arreglos.js
+++ Unidad08/leccion_01_generando_arreglos.js
@@ -4,7 +4,8 @@ console.log(frutas);
 let numeros = new Array(1, 2, 3, 4, 5);
 console.log(numeros);
 
-let resultado = Array.from('mira el cielo', (caracter) => caracter.toUpperCase());
+let resultado = Array.from('mira el cielo', 
+(caracter) => caracter.toUpperCase());
 console.log(resultado);
 
 let colores = ['rojo', 'verde', ...['azul', 'amarillo']];
