e3a79513853dba7dd2abc89475d663bef8aace75
Natalia C <46870371+nlunaty@users.noreply.github.com>
2023-10-07 12:22:30 +0100

    fix

--- Unidad08/leccion_07_buscar_datos_arreglo.js
+++ Unidad08/leccion_07_buscar_datos_arreglo.js
@@ -13,34 +13,34 @@ let frutas = ['manzana', 'banana', 'pera', 'melón', 'banana'];
 let indiceBanana = frutas.indexOf('banana');
 let ultimoIndiceBanana = frutas.lastIndexOf('banana');
 
-console.log(indiceBanana); // Output: 1
-console.log(ultimoIndiceBanana); // Output: 1
+console.log(indiceBanana); // Salida: 1
+console.log(ultimoIndiceBanana); // Salida: 1
 
-console.log(personas.indexOf({ nombre: 'eli' })); // Output: -1
-console.log(personas.lastIndexOf({ nombre: 'eli' })); // Output: -1
+console.log(personas.indexOf({ nombre: 'eli' })); // Salida: -1
+console.log(personas.lastIndexOf({ nombre: 'eli' })); // Salida: -1
 
-console.log(personas.indexOf(personas[3])); // Output: 3
-console.log(personas.lastIndexOf(personas[1])); // Output: 1
+console.log(personas.indexOf(personas[3])); // Salida: 3
+console.log(personas.lastIndexOf(personas[1])); // Salida: 1
 
 // Metodo includes: determina si el arreglo contiene el elemento buscado
 // Devuelve un valor booleano
-console.log(frutas.includes('melón')); // Output: true
-console.log(frutas.includes('sandía')); // Output: false
+console.log(frutas.includes('melón')); // Salida: true
+console.log(frutas.includes('sandía')); // Salida: false
 
-console.log(personas.includes({ nombre: 'eli' })); // Output: false
-console.log(personas.includes(personas[3])); // Output: true
+console.log(personas.includes({ nombre: 'eli' })); // Salida: false
+console.log(personas.includes(personas[3])); // Salida: true
 
 // Método find: retorna el elemento que coincida con la búsqueda
-console.log(frutas.find(fruta => fruta === 'melón')); // Output: 'melón'
-console.log(frutas.find(fruta => fruta === 'sandía')); // Output: undefined
+console.log(frutas.find(fruta => fruta === 'melón')); // Salida: 'melón'
+console.log(frutas.find(fruta => fruta === 'sandía')); // Salida: undefined
 
-console.log(personas.find(persona => persona.nombre === 'eli')); // Output: { nombre: 'eli' }
+console.log(personas.find(persona => persona.nombre === 'eli')); // Salida: { nombre: 'eli' }
 
 // Método findIndex: retorna el elemento que coincida con la búsqueda
-console.log(frutas.findIndex(fruta => fruta === 'melón')); // Output: 3
-console.log(frutas.findIndex(fruta => fruta === 'sandía')); // Output: -1
+console.log(frutas.findIndex(fruta => fruta === 'melón')); // Salida: 3
+console.log(frutas.findIndex(fruta => fruta === 'sandía')); // Salida: -1
 
-console.log(personas.findIndex(persona => persona.nombre === 'eli')); // Output: 0
+console.log(personas.findIndex(persona => persona.nombre === 'eli')); // Salida: 0
 
 // Utilizando un loop:
 let encontrada = false;
