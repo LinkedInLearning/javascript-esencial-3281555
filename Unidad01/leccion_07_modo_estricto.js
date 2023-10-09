313f40134a96c861d87a6a46a8b9c3348118f1f5
Natalia C <46870371+nlunaty@users.noreply.github.com>
2023-10-09 23:00:28 +0100

    unit11, 12, lesson 1-7

---
+++ Unidad01/leccion_07_modo_estricto.js
@@ -0,0 +1,26 @@
+/**
+ * El modo estricto en JavaScript es una forma
+ * de habilitar un conjunto más estricto
+ * de reglas para escribir código JavaScript.
+ * Ayuda a detectar errores comunes y prevenir
+ * ciertos tipos de errores.
+ * Se introdujo en ECMAScript 5 (ES5) para proporcionar
+ * un entorno de codificación más sólido y seguro.
+ */
+
+'use strict';
+
+// Previene asignar valores a variables no declaradas anteriormente:
+variableSinDeclaracion = 17;
+
+// Previeve la reasignación de valores globales:
+NaN = 10;
+
+//Previene parámetros duplicados en funciones:
+function suma(x, x) {
+    return x + x;
+}
+
+// No permite eliminar variables, funciones o argumentos de funciones
+var x = 10;
+delete x;
---
+++ Unidad12/logica.js
@@ -0,0 +1,57 @@
+// Función se ejecuta cuando se hace click en alguno de los robots:
+function clickEnRobot(numRobot){
+  console.log(`El robot #${numRobot} fué presionado`);
+}
+
+// Función se ejecuta cuando se hace envía el formulario:
+function validarFormulario() {
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const patronEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(!patronEmail.test(email)){
+        alert("El correo electrónico no es válido");
+        return false;
+    }
+  
+    if (nombre == "" || apellido == "" || email == "" || password == "") {
+      alert("Todos los campos deben rellenarse");
+      return false;
+    }
+  
+    return true;
+  }
+
+// Ejemplos de diferentes tipos de eventos:
+
+document.addEventListener('scroll', evento => {
+    console.log('Scroll en la página', evento);
+});
+
+window.addEventListener('resize', evento => {
+  console.log(evento);
+  console.log('Cambio de tamaño de la página', window.innerWidth, window.innerHeight);
+});
+
+document.getElementById("nombre").addEventListener('input', evento => {
+    console.log(evento);
+    console.log('Tecleo del nombre', evento.data);
+});
+
+// Ejemplo removeEventListener:
+let contador = 0;
+const escuchadorClickPagina = evento => {
+  console.log('Click en la página', evento);
+  contador++;
+  if(contador > 5){
+    document.removeEventListener('click', escuchadorClickPagina);
+  }
+}
+
+document.addEventListener('click', escuchadorClickPagina);
+
+
+  
